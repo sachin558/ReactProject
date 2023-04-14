03884734438103da08bc0c61c7960348b111cdfe
sachin558 <sachinkumar0126628@gmail.com>
2023-04-14 19:17:03 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PreNavbar from './componets/PreNavbar';
+import Navbar from "./componets/Navbar.js";
+import { BrowserRouter as Router } from 'react-router-dom';
+import Slider from "./componets/Slider.js"
+import data from "./data/data.json"
+import Offers from "./componets/Offers.js"
+import Heading from "./componets/Heading.js";
+import StarProduct from "./componets/StarProduct.js";
+import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.js"
+import ProductReviews from "./components/ProductReviews.js"
+import Videos from "./components/Videos.js"
+import Banner from "./components/Banner.js"
+import Footer from "./components/Footer.js"
+import NavOptios from "./components/NavOptios.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <PreNavbar></PreNavbar>
+      <Navbar></Navbar>
+      
+      <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/>
+
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCT "></Heading>
+      <StarProduct starProduct={data.starProduct} />
+      <Heading text="HOT ACCESSORIS "></Heading>
+      <HotAccessoriesMenu></HotAccessoriesMenu>
+      <Heading text="PRODUCT REVIEWS "></Heading>
+      <ProductReviews productReviews={data.productReviews}/>
+      <Heading text="VIDEOS" />
+      <Videos videos={data.videos} />
+      <Heading  text="IN THE PRESS"/>
+      <Banner  banner={data.banner}/>
+      <Footer footer={data.footer} />
+    </Router>
   );
 }
 
